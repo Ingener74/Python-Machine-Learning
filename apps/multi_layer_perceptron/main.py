a5fc0ae62ede3a313a30e8aa124f545152260707
Ingener <ingener@ingenerpc.(none)>
2013-02-08 00:53:10 +0600

    added multi layer perceptron test

---
+++ apps/multi_layer_perceptron/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+class Layer:
+	def __init__(self, inputs, neurons):
+		self._neurons = neurons
+		self._inputs = inputs + 1
+		self._weights = np.random.rand(self._neurons*self._inputs).reshape(self._neurons, self._inputs)
+	def printl(self):
+		print 'neurons = ', self._neurons, ', inputs = ', self._inputs
+		print 'weights:\n', self._weights
+	def predict(self, input):
+		return self._weights * input
+		pass
+
+def main():
+	print 'Multi layer perceptron'
+	l1 = Layer(2, 2)
+	l2 = Layer(2, 1)
+	l1.printl()
+	l2.printl()
+	
+	print 'output l1 = ', l1.predict(np.array([[1],[0]], 'double'))
+	pass
+
+if __name__ == '__main__':
+	main()
