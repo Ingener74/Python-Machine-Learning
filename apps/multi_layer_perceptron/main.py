fba912e61c9136e19065d00e5d3860a871953694
Ingener74 <shnaiderpasha@gmail.com>
2013-03-28 00:48:24 +0600

    some fix

--- apps/multi_layer_perceptron/main.py
+++ apps/multi_layer_perceptron/main.py
@@ -7,15 +7,15 @@ class Layer:
 		self._neurons = neurons
 		self._inputs = inputs + 1
 		self._weights = np.random.rand(self._neurons, self._inputs)
-		print "w = \n", self._weights.type
+		print "w = \n", self._weights.shape
 	def printl(self):
 		print 'neurons = ', self._neurons, ', inputs = ', self._inputs
 		print 'weights:\n', self._weights
 	def predict(self, input):
-		print "predict input = \n", input		
+		print "predict input = \n", input
 		input_t = input
 		input_t
-		print "predict input_t = \n", input_t 
+		print "predict input_t = \n", input_t
 		#return np.dot(self._weights, input)
 		pass
 
@@ -25,7 +25,7 @@ def main():
 	l2 = Layer(2, 1)
 	l1.printl()
 	l2.printl()
-	
+
 	print 'output l1 = ', l1.predict(np.array([[1],[0]], 'double'))
 	pass
 
