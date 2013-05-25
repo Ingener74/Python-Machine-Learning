4b4535c6cbc1567a5f426e9df823e23f7e3f1ff6
Ingener74 <shnaiderpasha@gmail.com>
2013-05-25 12:42:20 +0600

    some fix and chainges

--- multi_layer_perceptron.py
+++ multi_layer_perceptron.py
@@ -58,9 +58,27 @@ class NeuroNetIO(object):
 		self.file.close()
 		return x
 
+class TermCriteria(object):
+	""" Termination criteria class for NeuroNet Trainers """
+	CRIT_ERROR = 0
+	CRIT_ITER  = 1
+	def __init__(self, criteria, maxIteration, desiredError):
+		self.criteria = criteria
+		self.maxIteration = maxIteration
+		self.desiredError = desiredError
+		if self.criteria == self.CRIT_ERROR:
+			print 'desired error criteria, with error = ', self.desiredError
+		elif self.criteria == self.CRIT_ITER:
+			print 'max iteration criteria, with max iteration = ', self.maxIteration
+		else:
+			print 'unknown criteria'
+
 class BackPropTrainer(object):
 	def __init__(self):
 		print 'BackProp trainer'
+
+	def train(self, nn, termCrit):
+		pass
 		
 def mode_0():
 	print 'load and use NeuroNet'
@@ -72,12 +90,14 @@ def mode_1():
 	nn.printnn()
 
 	sys.stdout.write('train NeuroNet... ')
-	print('done')
+	bpt = BackPropTrainer()
+	bpt.train(nn, TermCriteria(TermCriteria.CRIT_ERROR, 1000, 0.001))
+	print 'done'
 	
 	sys.stdout.write('save NeuroNet...')
 	nnio = NeuroNetIO('nn.dat')
 	nnio.save(nn)
-	print('done')
+	print 'done'
 
 def mode_2():
 	print 'for future'
