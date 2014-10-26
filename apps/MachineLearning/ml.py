8d0ee4f4af0782c96f09a65770698cd9d03c7ec1
ShnaiderPavel <shnaiderpasha@gmail.com>
2014-10-26 23:16:40 +0400

    last changes

--- apps/MachineLearning/ml.py
+++ apps/MachineLearning/ml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from PySide.QtCore import QObject
 try:
     from PySide import QtCore, QtGui
     from ui_mainform import Ui_MainWindow
@@ -11,16 +12,21 @@ except():
     sys.exit(1)
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
-	def __init__(self, parent=None):
-		super(MainWindow, self).__init__(parent)
-		self.ui = Ui_MainWindow()
-		self.ui.setupUi(self)
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        QObject.connect(self.ui.pushButton, QtCore.SIGNAL('clicked()'), self, QtCore.SLOT('button1()'))
+        
+    def button1(self):
+        print "button 1"
 
 def main():
-	app = QtGui.QApplication(sys.argv)
-	mw = MainWindow()
-	mw.show()
-	sys.exit(app.exec_())
-	
+    app = QtGui.QApplication(sys.argv)
+    mw = MainWindow()
+    mw.show()
+    sys.exit(app.exec_())
+    
 if __name__ == '__main__':
-	main()
+    main()
